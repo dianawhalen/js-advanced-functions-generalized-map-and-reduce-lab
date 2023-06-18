f735522816872fc7832cad9a63a47de0f5256ec3
dianawhalen <whalendianaj@gmail.com>
2023-06-17 19:50:31 -0600

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(sourceArray, func) {
+  let result = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    result.push(func(sourceArray[i]));
+  }
+  return result;
+}
+
+function reduce(sourceArray, func, startingValue) {
+  let result = startingValue !== undefined ? startingValue : sourceArray[0];
+  let i = startingValue !== undefined ? 0 : 1;
+
+  for (; i < sourceArray.length; i++) {
+    result = func(sourceArray[i], result);
+  }
+  return result;
+}
